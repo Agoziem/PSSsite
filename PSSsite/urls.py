97a7e57e23fde9aadc9fa52c0d6f4fabb997452d
Agoziem <chiagoziendukwe90@gmail.com>
2024-02-06 15:41:47 -0800

    changes

--- PSSsite/urls.py
+++ PSSsite/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('TeachersPortal/', include('TeachersPortal.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header='PSS Umunya'
 admin.site.index_title='Site Administration'
